67f05dac9d4b9c0c30cbd07404d1d55a58417215
bob <bob62kelly@gmail.com>
2023-06-07 11:32:11 -0500

    test db

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,20 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import { db } from "@vercel/postgres";
+import { NextApiRequest, NextApiResponse } from "next";
 
-type Data = {
-  name: string
-}
-
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
+export default async function handler(
+    request: NextApiRequest,
+    response: NextApiResponse
 ) {
-  res.status(200).json({ name: 'John Doe' })
+    const client = await db.connect();
+
+    try {
+        await client.sql`CREATE TABLE Pets ( Name varchar(255), Owner varchar(255) );`;
+        const names = ["Fiona", "Lucy"];
+        await client.sql`INSERT INTO Pets (Name, Owner) VALUES (${names[0]}, ${names[1]});`;
+    } catch (error) {
+        return response.status(500).json({ error });
+    }
+
+    const pets = await client.sql`SELECT * FROM Pets;`;
+    return response.status(200).json({ pets });
 }
